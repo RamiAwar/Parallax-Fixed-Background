1ed736e67e8401bf7046cc60c8f490ff5c3a3fb9
RamiAwar <rami.awar.ra@gmail.com>
2015-08-12 14:14:06 +0300

    Upload Files

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-$(".face").hover(
-	function(){ $(this).find(".info").addClass('hoverInfo');
-				$(this).find(".mask").addClass('hoverMask'); },
-	function(){ $(this).find(".info").removeClass('hoverInfo');
-				$(this).find(".mask").removeClass('hoverMask'); }
-);
+
+
+
+$(window).scroll(function(){
+
+	var scrollVal = $(this).scrollTop();
+
+
+	//400 -> 900 needs to become a value between 0 -> 1
+	if ( scrollVal > 650){
+		$('.mask').css({
+			'opacity': (scrollVal - 650)/500
+		});
+	}
+
+
+	//1800 -> 2400 needs to become a value betwen -500 and 0
+	var offset = Math.min(0, -500 + ((scrollVal - 1800)/600)*500 );	
+
+	$('.talk1').css({
+		'transform':'translate(' + offset + 'px, ' + offset/5 + 'px)'
+	});
+
+	$('.talk2').css({
+		'transform':'translate(' + 0 + 'px, ' + offset/2 + 'px)'
+	});
+
+	$('.talk3').css({
+		'transform': 'translate(' + Math.abs(offset) + 'px, ' + offset/5 + 'px)'
+	});
+
+	
+
+
+
+
+
+});
+
+
+
